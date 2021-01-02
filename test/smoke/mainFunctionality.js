7c84334cd1fb3a5d0681708d86d605d26a0316cb
OlgaRozenberg <froggi3377@gmail.com>
2021-01-01 21:54:23 -0800

    added mainFunctionality.js

--- test/smoke/mainFunctionality.js
+++ test/smoke/mainFunctionality.js
@@ -1,13 +1,8 @@
 const sel = require('../../data/selectors.json');
-
 const exp = require('../../data/expected.json');
-
 const data = require('../../data/testData.json');
-
 const inputValues4 = require('../../helpers/inputValues4');
-
 const inputValues4click = require('../../helpers/inputValues4click');
-
 
 describe('Checking the main functionality', function () {
 
