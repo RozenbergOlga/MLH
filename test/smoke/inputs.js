e8538fa7ea61bcbc5fd3ae8b71415e3122bf849e
OlgaRozenberg <froggi3377@gmail.com>
2021-07-03 13:09:40 -0700

    inputs.js

--- test/smoke/inputs.js
+++ test/smoke/inputs.js
@@ -3,31 +3,38 @@ const exp = require('../../data/expected.json');
 
 describe('Inputs', function () {
     describe('Inputs are displayed', function () {
+
         it('TC-014 name ', function () {
             browser.url(sel.homePage);
             const name = $(sel.name).isDisplayed();
             expect(name).toEqual(true);
         });
+
         it('TC-015 gender he', function () {
             const gender = $$(sel.gender)[0].isDisplayed();
             expect(gender).toEqual(true);
         });
+
         it('TC-016 gender she', function () {
             const gender = $$(sel.gender)[1].isDisplayed();
             expect(gender).toEqual(true);
         });
+
         it('TC-017 gender it', function () {
             const gender = $$(sel.gender)[2].isDisplayed();
             expect(gender).toEqual(true);
         });
+
         it('TC-018 age', function () {
             const age = $(sel.age).isDisplayed();
             expect(age).toEqual(true);
         });
+
         it('TC-019 story', function () {
             const story = $(sel.story).isDisplayed();
             expect(story).toEqual(true);
         });
+
         it('TC-020 Create ', function () {
             const button = $(sel.label).isDisplayed();
             expect(button).toEqual(true);
