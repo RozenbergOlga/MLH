748b3b37588077bfc294000429cf983c03265454
OlgaRozenberg <froggi3377@gmail.com>
2021-10-13 20:57:59 -0700

    inputValues4.js

--- helpers/inputValues4.js
+++ helpers/inputValues4.js
@@ -1,7 +1,6 @@
 const sel = require('../data/selectors.json');
 
 function inputValues4(name, gender, age, storyType){
-
     $(sel.name).setValue(name);
     $$(sel.gender)[gender].click();
     $(sel.age).setValue(age);
